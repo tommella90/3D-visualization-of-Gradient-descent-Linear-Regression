893c897e9ef45287a459692f19804159d8fbc22e
tommaso.ramella <tommaso.ramella90@gmail.com>
2023-02-06 01:41:13 +0100

    corrections

---
+++ create_data.py
@@ -0,0 +1,120 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+import random 
+
+n_sample = op('parameters').par.value0
+noise = op('parameters').par.value1
+slope_interval = op('parameters').par.value2
+
+x = np.arange(n_sample)/n_sample
+slope = random.randint(10, slope_interval)
+bias = np.random.uniform(-10, noise, size=(n_sample,))
+y = (slope * x + bias)/100
+
+
+# random initial parameters
+b, m = random.randint(-150,150)/100, random.randint(-100,100)/100
+
+y_pred = b + m*x
+errors = (y - y_pred)**2
+
+
+# transfer data to td
+op('data/data_storage_x').unstore('*')
+op('data/data_storage_x').store('num_vars', x)
+
+op('data/data_storage_y').unstore('*')
+op('data/data_storage_y').store('num_vars', y)
+
+op('intercept').unstore('*')
+op('intercept').store('intercept', b)
+
+op('coefficient').unstore('*')
+op('coefficient').store('slope', m)
+
+op('y_pred').unstore('*')
+op('y_pred').store('y_pred', y_pred)
+
+op('ssr_').unstore('*')
+op('ssr_').store('errors', errors)
+
+
+"""
+op('3d_space').clear()
+intercept, coefficient, y_pred = [], [], []
+
+for i in range(-99, 101):
+	intercept_i = i/100
+
+	for c in range(-99, 101):
+		coefficient_i = c/100
+
+		prediction = intercept_i + x*coefficient_i
+		errors = ((y - prediction).sum())**2
+		row = [intercept_i, coefficient_i, errors]
+		op('3d_space').appendRow(row)
+
+"""
+
+# crate 3d shape 
+intercept, coefficient, errors = [], [], []
+my_range = 100
+steps = 1
+for intercept_i in range(-my_range+steps, my_range, steps+steps): 
+	intercept_i = float(intercept_i)/100
+
+	for coefficient_i in range(-my_range+steps, my_range, steps+steps): 
+		coefficient_i = float(coefficient_i)/100
+
+		intercept.append(intercept_i)
+		coefficient.append(coefficient_i)
+
+		y_pred = []
+		for instance_x in x:
+			y_pred.append(intercept_i + coefficient_i*instance_x)
+		errors.append(((y_pred - y)**2).sum())
+
+
+errors = np.array(errors)
+ssr = errors / np.linalg.norm(errors)
+ssr = errors.sum() / x.shape[0] 
+
+op('xy_ssr').clear()
+
+op('coeff_3d').unstore('*')
+op('coeff_3d').store('coefficient', coefficient)
+
+op('interc_3d').unstore('*')
+op('interc_3d').store('interc_3d', intercept)
+
+op('ssr_3d').unstore('*')
+op('ssr_3d').store('ssr', errors)
+
+epoch=0
+op('epoch').clear()
+op('epoch').appendRow(epoch)
+
+op('total_errors').clear()
+op('total_errors').appendRow(ssr)
+
+# plot correct ols
+x = x.reshape(-1,1)
+reg = LinearRegression().fit(x, y)
+
+m_sklearn = reg.coef_[0]
+b_sklearn =  reg.intercept_
+print(m_sklearn, b_sklearn)
+
+op('row_global_minima').par.rowindexstart = m_sklearn
+op('row_global_minima').par.rowindexend = b_sklearn
+
+op('ols_sklearn').clear()
+op('ols_sklearn').appendRow([m_sklearn, b_sklearn])
+
+
+op('info').par.value0 = m
+op('info').par.value1 = m_sklearn
+op('info').par.value2 = b
+op('info').par.value3 = b_sklearn
+op('info').par.value4 = ssr
+op('info').par.value5 = 0
---
+++ gradient_descent.py
@@ -0,0 +1,66 @@
+import numpy as np
+
+x, y = [], []
+for i in range(1, op('xy').numRows):
+	inst_x = float(op('xy')[i, 'x'])
+	x.append(inst_x)
+	inst_y = float(op('xy')[i, 'y'])
+	y.append(inst_y)
+
+x, y = np.array(x), np.array(y)
+b, m = float(op('bias_')[0,0]), float(op('coeff_')[0,0])
+learning_rate = .1
+N = x.shape[0]
+
+
+# y = mx + b
+der_m, der_b = [], []
+for x_i, y_i in zip(x, y):
+	y_pred = x_i*m + b
+	der_m.append((y_i - y_pred) * x_i)
+	der_b.append(y_i - y_pred)
+
+der_m = (-2/N)*(np.array(der_m).sum())
+der_b = (-2/N)*(np.array(der_b).sum())
+
+new_m = m - (learning_rate*der_m)
+new_b = b - (learning_rate*der_b)
+
+y_pred = new_b + new_m*x
+errors = (y_pred - y)**2
+errors = np.array(errors)
+ssr = errors.sum() / x.shape[0]
+
+print("true coeff=", op('ols_')[1,0], 
+	"\nactual coeff=", new_m,
+	"\ntrue intercept=:", op('ols_')[1,1],
+	"\nactual intercept=", new_b,
+	"\nssr=", ssr, 
+	"\nepoch=", op('epoch')[op('epoch').numRows-1, 0])
+
+op('info').par.value0 = op('ols_')[1,0]
+op('info').par.value1 = new_m
+op('info').par.value2 = op('ols_')[1,1]
+op('info').par.value3 = new_b
+op('info').par.value4 = ssr
+op('info').par.value5 = op('epoch')[op('epoch').numRows-1, 0]
+
+# weights and biases
+op('intercept').unstore('*')
+op('intercept').store('intercept', new_b)
+
+op('coefficient').unstore('*')
+op('coefficient').store('coefficient', new_m)
+
+op('y_pred').unstore('*')
+op('y_pred').store('y_pred', y_pred)
+
+op('ssr_').unstore('*')
+op('ssr_').store('ssr', errors)
+
+n_rows = op('epoch').numRows
+epoch = op('epoch')[n_rows-1, 0]
+epoch+=1
+op('epoch').appendRow(epoch)
+op('total_errors').appendRow(ssr)
+
